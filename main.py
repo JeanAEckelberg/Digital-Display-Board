364124a936ac3d141e8e74134a56adf91e17c672
Jean A. Eckelberg <fyrelynx11@gmail.com>
2023-11-09 15:23:10 -0600

    streamlined and refactored code. Made settings easier to modify without searching through the code made scaling adaptive

--- main.py
+++ main.py
@@ -2,42 +2,45 @@ import sys, pygame, utils.util
 
 pygame.init()
 
+# Config settings
 size = width, height = 1920, 1080
-speed = [2,2]
-black = 0, 0, 0
+
+top_offset = 100
+line_offset = 12
+lr_margin = 50
+
+line_color = '#DAA520'
+text_color = '#FFFFFF'
+bg_color = '#006B3C'
 
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-simple_font = pygame.font.Font(None, 50);
-# title = pygame.image.load("Title Logo.png")
-# titlerect = title.get_rect()
 
-text_surfaces_L = list()
-text_surfaces_R = list()
-line = simple_font.render('_'*190, False, 'Gray')
-for key, value in utils.util.read_text_to_dict().items():
-    text_surfaces_L.append(simple_font.render(key, False, 'White'))
-    text_surfaces_R.append(simple_font.render(value, False, 'White'))
+text_and_cost = utils.util.read_text_to_dict().items()
+rule_size = (height-top_offset) // len(text_and_cost)
+font_size = round(rule_size * .84)
+simple_font = pygame.font.Font(None, font_size)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-        
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE: sys.exit()
-    
 
-    # DO ACTIONS
-    screen.fill((0,0,0))
+    screen.fill(bg_color)
 
-    for index, surface in enumerate(text_surfaces_L):
-        screen.blit(surface, (200, 300+60*index))
-        screen.blit(line, (0, 312+60*index))
 
-    for index, surface in enumerate(text_surfaces_R):
-        screen.blit(surface, (1520, 300+60*index))
-    # screen.blit(title, titlerect)
+    for index, (surface_l, surface_r) in enumerate(text_and_cost):
+        # right align rect
+        right_text = simple_font.render(surface_r, False, text_color)
+        temp_rect = right_text.get_rect()
+        temp_rect.topright = (width-lr_margin, top_offset + rule_size * index)
+
+        screen.blit(simple_font.render(surface_l, False, text_color), (lr_margin, top_offset + rule_size * index))
+        screen.blit(right_text, temp_rect)
+        screen.blit(simple_font.render('_' * 255, False, line_color), (0, top_offset+line_offset + rule_size * index))
 
     # Render Frame
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(1)
--- utils/util.py
+++ utils/util.py
@@ -2,24 +2,4 @@
 
 
 def read_text_to_dict():
-    strDict = dict()
-    with open("menu.txt", 'r') as f:
-        for line in f:
-            x,y = line.split(', ')
-            strDict[x]=y.strip('\n')
-
-    return strDict
-
-
-def pull_from_github():
-    # Close Main Program
-    # Pull Repository from github using oauth
-    # Restart Main Program 
-    return None
-
-if __name__ == "__main__":
-    temp = read_text_to_dict()
-    for key, value in temp.items():
-        print(f'{key}: {value}')
-
-    pull_from_github()
+    return dict([[x.strip().strip('\n') for x in line.split(',')] for line in open("menu.txt", 'r').readlines()])
